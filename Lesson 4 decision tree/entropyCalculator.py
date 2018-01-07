b6c5852a305123de1f14f39d036bb4b1dea823c5
Andrew Xu <andrewxu@gatech.edu>
2018-01-06 21:06:58 -0700

    Add information gain function in entropyCalculator

--- Lesson 4 decision tree/entropyCalculator.py
+++ Lesson 4 decision tree/entropyCalculator.py
@@ -1,5 +1,9 @@
 import math
-pi=0.333
+pi=0.5
 rest = 1-pi
 entropy = -pi *math.log(pi,2) - rest * math.log(rest,2)
-print entropy
+print "entropy:", entropy
+
+inforGain = 1- entropy
+
+print "information gain: ", inforGain
